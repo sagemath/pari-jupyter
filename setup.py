dabae21f9bf6a3058b5c8177295a5658c10ca9bd
Jeroen Demeyer <jdemeyer@cage.ugent.be>
2016-02-18 16:11:55 +0100

    Allow installing without Cython

--- setup.py
+++ setup.py
@@ -2,11 +2,29 @@
 
 import os
 from glob import glob
-from distutils.core import setup
-from Cython.Build import cythonize
+from distutils.core import setup, Extension
+from distutils.version import StrictVersion
 import PARIKernel
 
 kernelpath = os.path.join("share", "jupyter", "kernels", "pari_jupyter")
+
+USE_CYTHON = False
+
+try:
+    import Cython
+    if StrictVersion(Cython.__version__) >= StrictVersion("0.24.0a0"):
+        USE_CYTHON = True
+except Exception:
+    pass
+
+ext = ".pyx" if USE_CYTHON else ".c"
+
+extensions = [Extension("PARIKernel.kernel", ["PARIKernel/kernel"+ext]),
+              Extension("PARIKernel.io", ["PARIKernel/io"+ext])]
+
+if USE_CYTHON:
+    from Cython.Build import cythonize
+    extensions = cythonize(extensions)
 
 setup(
     name='pari_jupyter',
@@ -17,6 +35,6 @@ setup(
     license='GNU Public License (GPL) version 3 or later',
     url="https://github.com/jdemeyer/pari_jupyter",
     packages=['PARIKernel'],
-    ext_modules=cythonize("PARIKernel/*.pyx"),
+    ext_modules=extensions,
     data_files=[(kernelpath, glob("spec/*"))],
 )
